843a42672d46e6173be2d3742a4b5c07007c1206
Jacqueline Potts <jacqueline.a.potts@gmail.com>
2015-08-25 16:44:19 -0400

    adds more seeds

--- bandtwo/db/seeds.rb
+++ bandtwo/db/seeds.rb
@@ -6,8 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Band.create({name: "The Kills",genre: "Alternative"})
+kills = Band.create({name: "The Kills",genre: "Alternative"})
+joel = Band.create({name: "Billy Joel",genre: "Classic Rock"})
+shins = Band.create({name: "The Shins",genre: "Alternative"})
 
-Venue.create({name: "Jones Beach",city: "New York",state: "NY",family_friendly: true})
 
-Event.create({date: "8/1/2015",alcohol_served: false,band: Band.first,venue: Venue.first})
+jones = Venue.create({name: "Jones Beach",city: "New York",state: "NY",family_friendly: true})
+msg = Venue.create({name: "MSG",city: "New York",state: "NY",family_friendly: true})
+big = Venue.create({name: "The Big Theater",city: "Atlanta",state: "GA"})
+
+Event.create({date: "8/1/2015",alcohol_served: false,band: kills,venue: jones})
+Event.create({date: "9/15/2015",alcohol_served: false,band: joel,venue: msg})
+Event.create({date: "10/10/2015",band: shins,venue: big})
