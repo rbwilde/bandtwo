24bf7bfc5a1e63c8614338f0eb7481c3a054544d
Jacqueline Potts <jacqueline.a.potts@gmail.com>
2015-08-25 16:22:36 -0400

    established routes

--- bandtwo/config/routes.rb
+++ bandtwo/config/routes.rb
@@ -14,6 +14,13 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  shallow do
+    resources :venues do
+    resources :events
+    end
+  resources :bands
+  end
+
   # Example resource route with options:
   #   resources :products do
   #     member do
